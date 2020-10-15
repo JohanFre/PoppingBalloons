8bbf3e4c7c84f540dc175aa0b316cfb7d802917a
JohanFre <70636307+JohanFre@users.noreply.github.com>
2020-10-15 19:35:25 +0200

    Last touch

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const isBalloon = document.querySelectorAll("balloon")
+const BalloonsEl = document.querySelectorAll("balloon");
 
 // Function for mouseover
 document.addEventListener("mouseover", (event) => {
-    const isBalloon = event.target.classList.contains("balloon");
-    const isPopped = event.target.classList.contains("popped")
-    
-// If mouseover balloon, change balloon to "POP!" string
-    if (isBalloon) {
-        event.target.classList.add("popped");
-        const poppedCount = document.querySelectorAll(".balloon.popped").length;
- 
-// Counts so when all balloons are popped. Clears balloons and "YAY!" message appears        
-        if (poppedCount === 24) {
-            const balloons = document.querySelector(".wrapper");
+  const isBalloon = event.target.classList.contains("balloon");
+  const isPopped = event.target.classList.contains("popped");
 
-            balloons.innerHTML = ""; //Clears all ballons
+  // If mouseover balloon, change balloon to "POP!" string
+  if (isBalloon) {
+    event.target.classList.add("popped");
+    const poppedCount = document.querySelectorAll(".balloon.popped").length;
 
-            //Creates new h1 tag
-            const h1 = document.createElement('h1'); 
-            h1.textContent = "YAY!";
-            h1.classList.add("congratsMessage");
-            balloons.appendChild(h1);
-        }
+    // Counts so when all balloons are popped. Clears balloons and "YAY!" message appears
+    if (poppedCount === 24) {
+      const balloons = document.querySelector(".wrapper");
+
+      balloons.innerHTML = ""; //Clears all ballons
+
+      //Creates new h1 tag
+      const h1 = document.createElement("h1");
+      h1.textContent = "YAY!";
+      h1.classList.add("congratsMessage");
+      balloons.appendChild(h1);
     }
-
-})
+  }
+});
